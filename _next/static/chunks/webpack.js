d0a4d75184d04ebadb6e77ab65338feb7fec5c6e
Wilson <wilson@policypal.com>
2020-11-10 22:19:24 +0800

    (chore) - Update web redirect modal

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7e6c20de57b644820f20";
+/******/ 	var hotCurrentHash = "865ebe6cc9cd8126f914";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
