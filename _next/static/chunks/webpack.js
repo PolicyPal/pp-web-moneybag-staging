5639c97a44090350820c656d4a2704f223079b9c
Wilson <wilson@policypal.com>
2020-11-10 22:35:45 +0800

    (chore) - Update redirect app store & deeplink

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "865ebe6cc9cd8126f914";
+/******/ 	var hotCurrentHash = "d12043b2f9e9855b0ec6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
