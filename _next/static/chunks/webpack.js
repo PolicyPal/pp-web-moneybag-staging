c6752e4609e17a9ba106bdc24530a597cfbda6d3
Wilson <wilson@policypal.com>
2020-11-13 13:07:10 +0800

    (chore) - Fix prepend issue

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6b2b8831c4c6c89123c7";
+/******/ 	var hotCurrentHash = "916a102b07db2ddc5a0e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
