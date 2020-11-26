598ebe5f0b27dc93a597f30038b6bc9117c38209
Wilson <wilson@policypal.com>
2020-11-26 11:45:48 +0800

    (chore) - Add Nickname in friendlist

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "916a102b07db2ddc5a0e";
+/******/ 	var hotCurrentHash = "bd2a9d9b606a0bd28747";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
