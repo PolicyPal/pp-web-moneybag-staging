2780a13b6ee97eefee71b6c0df61338d60a7b546
Wilson <wilson@policypal.com>
2021-01-06 18:04:02 +0800

    (chore) - Update friendlist profile pic

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "01406093307c592cb283";
+/******/ 	var hotCurrentHash = "a8d7cf7946793de20236";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
