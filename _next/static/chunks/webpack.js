4c7c4ef949646ead7896593caea35391ed19d339
Wilson <wilson@policypal.com>
2021-01-08 15:24:31 +0800

    (chore) - Update profile icon color

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a8d7cf7946793de20236";
+/******/ 	var hotCurrentHash = "a7acd1096ed09fd58608";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
