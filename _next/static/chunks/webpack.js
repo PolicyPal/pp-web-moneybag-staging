afba6c8e34acbdd6ab7aa93c0acc2fdd05be82cb
Wilson <wilson@policypal.com>
2021-01-15 16:49:20 +0800

    (chore) - Update Landing

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a7acd1096ed09fd58608";
+/******/ 	var hotCurrentHash = "efcb470ab7814ec8082c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
