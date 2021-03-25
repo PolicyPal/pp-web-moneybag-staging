56c66651817ca877af75983447683434ee99cd7c
Wilson <wilson@policypal.com>
2021-03-25 12:56:12 +0800

    (chore) - Update navbar

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a994325587a1897b987";
+/******/ 	var hotCurrentHash = "fc5f58cafb0bd079ac06";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
